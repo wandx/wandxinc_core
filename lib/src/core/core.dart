a2c57f790726588a43108096836495e0a30080db
wandypurnomo <wandypurnomo92@gmail.com>
2024-07-15 11:13:21 +0700

    update version and export

--- lib/src/core/core.dart
+++ lib/src/core/core.dart
@@ -1,5 +1,6 @@
 export 'exception/base_exception.dart';
 export 'extensions/num_ext.dart';
 export 'extensions/response_ext.dart';
+export 'extensions/validator_ext.dart';
 export 'http/remote_client.dart';
 export 'use_case/base_use_case.dart';
