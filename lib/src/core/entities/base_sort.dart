cc8556a1988ca7fe6f14ff750742bab28a5741df
wandypurnomo <wandypurnomo92@gmail.com>
2025-07-01 23:24:25 +0700

    refactor - remove value on base_sort.dart - add code on base_exception.dart

--- lib/src/core/entities/base_sort.dart
+++ lib/src/core/entities/base_sort.dart
@@ -5,15 +5,11 @@ class BaseSort extends Equatable {
   /// BaseSort constructor
   const BaseSort({
     required this.key,
-    required this.value,
     this.isAscending = true,
   });
 
   /// The key of the sort
   final String key;
-
-  /// The value of the sort
-  final String value;
 
   /// The order of the sort
   final bool isAscending;
@@ -21,7 +17,6 @@ class BaseSort extends Equatable {
   @override
   List<Object?> get props => [
         key,
-        value,
         isAscending,
       ];
 }
--- lib/src/core/exception/base_exception.dart
+++ lib/src/core/exception/base_exception.dart
@@ -1,10 +1,16 @@
 /// Base exception class for all exceptions in the application.
 abstract class BaseException implements Exception {
   /// Constructor for the base exception class.
-  BaseException({required this.message});
+  BaseException({
+    required this.message,
+    this.code = 0,
+  });
 
   /// The error message.
   final String message;
+
+  /// The error code.
+  final int code;
 
   @override
   String toString() => message;
